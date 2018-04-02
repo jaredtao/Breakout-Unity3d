8592840e48ec02f523c7fbd06017fbdaa05061bf
jiawentao <wentaojia2014@163.com>
2018-04-03 01:18:30 +0800

    init version

---
+++ Assets/Script/DeadZone.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour {
+	void OnTriggerEnter (Collider collider) {
+		GM.instance.LostLife ();
+	}
+}
